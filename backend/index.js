dce6500b34290dd68c1b083ff5e3424fcac15fd3
jatin4114 <jatinpanghal007@gmail.com>
2024-07-13 01:57:15 +0530

    directory structure

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const db = require('./db');
+const app= express();
+
+
+app.use(express.json());
+
+
+
+
+//bullshit goes here
+
+
+
+
+
+
+
+app.listen(5375 );
